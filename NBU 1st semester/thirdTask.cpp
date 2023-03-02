65f85712b7cecf1ef85e7d5ddd0acd52943d0928
Руслан Каписъски <89385864+RuslanKapisaski@users.noreply.github.com>
2023-03-02 11:58:09 +0200

    Add files via upload

--- NBU 1st semester/thirdTask.cpp
+++ NBU 1st semester/thirdTask.cpp
@@ -1,11 +1,54 @@
-#include <iostream>
-
+#include <iostream>;
 using namespace std;
 
 int main()
 {
-	cout << "C++'s key words are:" << endl;
-	cout << "auto, double, break, else, case, enum, char, extern, const, float, continue, for, default, goto, do,\nif, int, long, register, return, short, signed, sizeof, static, struct, switch, typedef, union, unsigned, \nvoid, volatile, while, asm, bool, catch, lass, const_cast, delete, dynamic_cast, explicit, false, friend, \ninline, mutable, namespace, new, operator, private, protected, public, reinterpret_cast, static_cast, template, \nthis, throw, true, try, typeid, typename, using, virtual, wchar_t" << endl;
+	cout << "Please, insert two numbers in order to get known about the shortened mathematical operations. " << endl;
+
+	int a = 0;
+    int b = 0;
+	int c = a;
+    int d = b;
+
+	cin >> a;
+	cin >> b;
+
+	cout<<"a++"<< endl;
+	a++;
+	cout << a << endl;
+	a--;
+
+	cout << "b++" << endl;
+	b++;
+	cout << b<< endl;
+	b--;
+
+	cout << "a+=b" << endl;
+	a += b;
+	cout << a << endl ;
+	a -= b;
+
+	cout << "a-=b" << endl;
+	a -= b;
+	cout << a << endl;
+	a += b;
+
+	cout << "a*=b" << endl;
+	a *= b;
+	cout << a << endl;
+	a /= b;
+
+	cout << "a/=b" << endl;
+	a /= b;
+	cout << a << endl;
+	a *= b;
+
+	
+
+
+
+
+
 
 	return 0;
 }
