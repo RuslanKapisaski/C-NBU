7454e9f1509b5ad5e667a1d6f4bdf8a693987844
Руслан Каписъски <89385864+RuslanKapisaski@users.noreply.github.com>
2023-03-02 12:05:45 +0200

    Update introductionOfMyself.cpp

--- NBU 1st semester/introductionOfMyself.cpp
+++ NBU 1st semester/introductionOfMyself.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "I am a student in New Bulgarian University\n";
-	cout << "My educational program is infromatics!\a" << endl;
+	cout << "My educational program is infromatics!" << endl;
 
 	return 0;
 }
