4ea0d711a45bff5059583e3b7d5cf55691086254
Руслан Каписъски <89385864+RuslanKapisaski@users.noreply.github.com>
2023-03-12 11:29:10 +0200

    Add files via upload

---
+++ NBU 1st semester/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <clocale>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "bulgarian");
+	int columns;
+	int counter;
+	std::cout << "Моля, въведете положитело число:" << endl;
+	cin >> columns;
+	int n = 0;
+
+	if (columns > 0)
+	{
+		for (size_t i = 0; i < columns + 1; i++)
+		{
+			int y = i;
+			if (i == 0)
+			{
+				for (size_t i = 0; i < columns; i++)
+				{
+					std::cout << 0;
+				}
+				std::cout << "" << endl;
+			}
+			else if (i > 0)
+			{
+				std::cout << 1;
+				while (y > 1)
+				{
+					//std::cout << i;
+					for (size_t z = 1; z < i; z++)
+					{
+						std::cout << z + 1;
+						y--;
+					}
+
+
+				}
+				n++;
+				for (size_t i = n; i < columns; i++)
+				{
+					std::cout << 0;
+				}
+				std::cout << "" << endl;
+
+			}
+
+		}
+
+
+	}
+	else
+	{
+		std::cout << "Въведохте невалидно число" << endl;
+	}
+
+	return 0;
+}
