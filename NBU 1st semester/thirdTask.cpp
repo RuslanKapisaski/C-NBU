976a5f4aba0eebd54ab1c3023ec4d4560c09c8ec
Руслан Каписъски <89385864+RuslanKapisaski@users.noreply.github.com>
2023-03-12 11:23:22 +0200

    Add files via upload

---
+++ NBU 1st semester/thirdTask.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "C++'s key words are:" << endl;
+	cout << "auto, double, break, else, case, enum, char, extern, const, float, continue, for, default, goto, do,\nif, int, long, register, return, short, signed, sizeof, static, struct, switch, typedef, union, unsigned, \nvoid, volatile, while, asm, bool, catch, lass, const_cast, delete, dynamic_cast, explicit, false, friend, \ninline, mutable, namespace, new, operator, private, protected, public, reinterpret_cast, static_cast, template, \nthis, throw, true, try, typeid, typename, using, virtual, wchar_t" << endl;
+
+	return 0;
+}
